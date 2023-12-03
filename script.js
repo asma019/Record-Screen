c7920ec365286a093a8f927ee65740a963b5db6b
Mehedi Hasan <asmaparvin019@gmail.com>
2023-12-03 17:16:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let mediaRecorder;
 let screenStream;
-let audioStream;
 let recordedChunks = [];
 
 const startButton = document.getElementById('start');
@@ -16,61 +15,45 @@ startButton.addEventListener('click', startRecording);
 stopButton.addEventListener('click', stopRecording);
 downloadButton.addEventListener('click', downloadRecording);
 
-async function startRecording() {
+function startRecording() {
   startButton.disabled = true;
   stopButton.disabled = false;
   recordedChunks = [];
 
-  // Capture video from screen
-  try {
-    screenStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-  } catch (err) {
-    console.error('Error accessing screen:', err);
-    return;
+  // Check if the getDisplayMedia method is available
+  if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
+    navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+      .then(stream => {
+        screenStream = stream;
+
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = function(event) {
+          if (event.data.size > 0) {
+            recordedChunks.push(event.data);
+          }
+        };
+
+        mediaRecorder.onstop = function() {
+          startButton.disabled = false;
+          stopButton.disabled = true;
+          downloadButton.disabled = false;
+
+          const videoBlob = new Blob(recordedChunks, { type: 'video/webm' });
+          videoElement.src = URL.createObjectURL(videoBlob);
+
+          // Stop both screen sharing and media recording streams
+          screenStream.getTracks().forEach(track => track.stop());
+        };
+
+        mediaRecorder.start();
+      })
+      .catch(err => {
+        console.error('Error accessing screen:', err);
+      });
+  } else {
+    alert('Screen recording is not supported on this device/browser.');
   }
-
-  // Capture audio from desktop (system audio)
-  try {
-    audioStream = await navigator.mediaDevices.getUserMedia({ audio: { mediaSource: 'audioCapture' } });
-  } catch (err) {
-    console.error('Error accessing audio:', err);
-    return;
-  }
-
-  // Capture audio from the microphone
-  try {
-    const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    audioStream.addTrack(micStream.getAudioTracks()[0]);
-  } catch (err) {
-    console.error('Error accessing microphone:', err);
-    return;
-  }
-
-  // Combine the video, desktop audio, and microphone audio streams
-  const combinedStream = new MediaStream([...screenStream.getTracks(), ...audioStream.getTracks()]);
-
-  mediaRecorder = new MediaRecorder(combinedStream);
-
-  mediaRecorder.ondataavailable = function(event) {
-    if (event.data.size > 0) {
-      recordedChunks.push(event.data);
-    }
-  };
-
-  mediaRecorder.onstop = function() {
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    downloadButton.disabled = false;
-
-    const videoBlob = new Blob(recordedChunks, { type: 'video/webm' });
-    videoElement.src = URL.createObjectURL(videoBlob);
-
-    // Stop all streams
-    screenStream.getTracks().forEach(track => track.stop());
-    audioStream.getTracks().forEach(track => track.stop());
-  };
-
-  mediaRecorder.start();
 }
 
 function stopRecording() {
